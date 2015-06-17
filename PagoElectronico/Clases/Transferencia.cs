a5e7fb030d99be4d00160c55ac7cd63c652eb44b
Guillermo Santillan <guillermosantillan1437847@yahoo.com>
2015-06-17 03:31:54 -0300

    Clase Transferencia y ABM transferencia por cuenta.
    Falta completar algunas cosas. Revisar!

---
+++ PagoElectronico/Clases/Transferencia.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using Conexion;
+using System.Configuration;
+using Excepciones;
+using Utilities;
+using System.Windows.Forms;
+
+namespace Clases
+{
+    public class Transferencia : Base
+    {
+        #region variables
+        List<SqlParameter> parameterList = new List<SqlParameter>();
+        #endregion
+
+        #region atributos
+
+        private Transferencia transferencia_id;
+        private Cuenta cuentaOrigen;
+        private Cuenta cuentaDestino;
+        private int importe;
+        private int costo;
+        private DateTime fecha;
+
+        #endregion
+
+        #region constructor
+
+        public Transferencia()
+        { 
+        
+        }
+
+        public Transferencia(Cuenta unaCuentaOrigen, Cuenta unaCuentaDestino)
+        {
+            this.cuentaOrigen = unaCuentaOrigen;
+            this.cuentaDestino = unaCuentaDestino;
+        }
+
+        #endregion
+
+        #region properties
+
+        public Cuenta CuentaOrigen
+        {
+            get { return cuentaOrigen; }
+            set { cuentaOrigen = value; }
+        }
+
+        public Cuenta CuentaDestino
+        {
+            get { return cuentaDestino; }
+            set { cuentaDestino = value; }
+        }
+
+        public Transferencia Transferencia
+        {
+            get { return transferencia_id; }
+            set { transferencia_id = value; }
+        }
+
+        public int Importe
+        {
+            get { return importe; }
+            set { importe = value; }
+        }
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        public int Costo
+        {
+            get { return costo; }
+            set { costo = value; }
+        }
+
+        #endregion
+
+        #region metodos publicos
+
+        public override string NombreTabla()
+        {
+            return "Transferencia";
+        }
+
+        public override string NombreEntidad()
+        {
+            return "Transferencia";
+        }
+
+        #endregion
+
+        public override void DataRowToObject(DataRow dr)
+        {
+            this.transferencia_id = Convert.ToInt32(dr[""]);
+            this.CuentaOrigen.cuenta_id = Convert.ToDouble(dr["transferencia_origen_cuenta_id"]);
+            this.CuentaDestino.cuenta_id = Convert.ToDouble(dr["transferencia_destino_cuenta_id"]);
+            this.Importe = Convert.ToInt32(dr["transferencia_importe"]);
+            this.Costo = Convert.ToInt32(dr["transferencia_costo"]);
+            this.Fecha = Convert.ToDateTime(dr["transferencia_fecha"]);
+        }
+    }
+}
--- PagoElectronico/PagoElectronico/Transferencias/Transferencias_Entre_Cuentas.cs
+++ PagoElectronico/PagoElectronico/Transferencias/Transferencias_Entre_Cuentas.cs
@@ -6,13 +6,26 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Configuration;
 using Clases;
+using Utilities;
 
 namespace PagoElectronico.Transferencias
 {
     public partial class Transferencias_Entre_Cuentas : Form
     {
         public Usuario unUsuario = new Usuario();
+
+        public Cliente unClienteOrigen = new Cliente();
+
+        public Cliente unClienteDestino = new Cliente();
+         
+        public Cuenta unaCuentaOrigen = new Cuenta();
+
+        public Cuenta unaCuentaDestino = new Cuenta();
+
+        public Transferencia unaTransferencia = new Transferencia();
+
 
 
         public Transferencias_Entre_Cuentas()
@@ -25,5 +38,147 @@ namespace PagoElectronico.Transferencias
             unUsuario = user;
             this.Show();
         }
+
+
+     
+
+        #region botones
+
+        private void Transferencias_Entre_Cuentas_Load(object sender, EventArgs e)
+        {
+            //cargar cmbClienteOrigen
+            DataSet dsClienteOrigen = this.ObtenerClientes();
+            DropDownListManager.CargarCombo(cmbClienteOrigen, dsClienteOrigen.Tables[0], "cliente_id", "cliente_nombre", false, "");
+
+            //cargar cmbClienteDestino
+            DataSet dsClienteDestino = unClienteOrigen.ObtenerTodosLosClientes(unUsuario.usuario_id);
+            DropDownListManager.CargarCombo(cmbClienteOrigen, dsClienteDestino.Tables[0], "cliente_id", "cliente_nombre", false, ""); 
+
+            
+        }
+ 
+        private void cmbClienteOrigen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //cargar CuentasClienteOrigen
+            DataSet ds = traerCuentasPorCliente(unClienteOrigen);
+
+        }
+
+        private void cmbClienteDestino_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //cargar CuentasClienteDestino
+            DataSet ds = traerCuentasPorCliente(unClienteDestino);
+        }
+
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (ValidarImporteNoVacio())
+            {
+                if (Convert.ToInt32(cmbCuentaOrigen.SelectedValue) == unaCuentaOrigen.cuenta_id &&
+                    Convert.ToInt32(cmbCuentaDestino.SelectedValue) == unaCuentaDestino.cuenta_id)
+                {
+                    MessageBox.Show("Se ha validado correctamente las cuentas", "Validacion Exitosa");
+
+                    realizarAccionesTransferencia();
+                }
+                else
+                {
+                    MessageBox.Show("Vuelva a seleccionar las cuentas", "Datos Incorrectos");
+                }
+            }
+        }
+
+        #endregion
+
+     #region funciones
+
+        public DataSet ObtenerClientes()
+        {
+
+            DataSet ds = new DataSet();
+            if (unUsuario.Rol.rol_id == 1)
+            {
+                DataSet dsClientes = unClienteOrigen.ObtenerTodosLosClientes(unUsuario.usuario_id);
+                ds = dsClientes;
+            }
+            else
+            {
+                DataSet dsClienteUsuario = unClienteOrigen.ObtenerClientesPorUsuarioID(unUsuario.usuario_id);
+                ds = dsClienteUsuario;
+            }
+
+            return ds;
+
+        }
+        
+        public DataSet traerCuentasPorCliente(Cliente unCliente)
+        {
+            Cuenta unaCuenta = new Cuenta(unCliente, unUsuario);
+            DataSet dsCuentas = unaCuenta.TraerCuentasActivasPorClienteID();
+            return dsCuentas;
+        }
+        
+        private bool ValidarImporteNoVacio()
+        {
+            string strErrores = "";
+            strErrores = Validator.ValidarNulo(txtImporte.Text, "Importe");
+            if (strErrores.Length > 0)
+            {
+                MessageBox.Show(strErrores);
+                txtImporte.Clear();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        private void realizarAccionesTransferencia()
+        {
+            int clienteOrigenID = Convert.ToInt32(cmbClienteOrigen.SelectedValue);
+            DataSet dsClienteOrigen = unClienteOrigen.TraerClientePorID(clienteOrigenID);
+            unClienteOrigen.DataRowToObject(dsClienteOrigen.Tables[0].Rows[0]);
+
+
+            double cuentaOrigenID = Convert.ToDouble(cmbCuentaOrigen.SelectedValue);
+            DataSet dsCuentaOrigen = unaCuentaOrigen.TraerCuentaPorCuentaID(cuentaOrigenID);
+            unaCuentaOrigen.DataRowToObject(dsCuentaOrigen.Tables[0].Rows[0]);
+
+            int clienteDestinoID = Convert.ToInt32(cmbClienteDestino.SelectedValue);
+            DataSet dsClienteDestino = unClienteDestino.TraerClientePorID(clienteDestinoID);
+            unClienteDestino.DataRowToObject(dsClienteDestino.Tables[0].Rows[0]);
+
+
+            double cuentaDestinoID = Convert.ToDouble(cmbCuentaDestino.SelectedValue);
+            DataSet dsCuentaDestino = unaCuentaDestino.TraerCuentaPorCuentaID(cuentaDestinoID);
+            unaCuentaDestino.DataRowToObject(dsCuentaDestino.Tables[0].Rows[0]);
+
+            int importe = Convert.ToInt32(txtImporte.Text);
+            if (importe <= unaCuentaOrigen.saldo)
+            {
+                generarTransferenciaExitosa();
+            }
+            else
+            {
+                MessageBox.Show("No tiene suficiente saldo para realizar la Transferencia. Por favor, vuelva a ingresar el importe", "Saldo Insuficiente");
+                txtImporte.Clear();
+            }  
+        }
+
+        private void generarTransferenciaExitosa()
+        {
+
+            unaTransferencia.CuentaOrigen = Convert.ToDouble(cmbCuentaOrigen.SelectedValue);
+            unaTransferencia.CuentaDestino = Convert.ToInt32(cmbCuentaDestino.SelectedValue);
+            unaTransferencia.Importe = Convert.ToInt32(txtImporte.Text);
+        
+        }
+     
+     #endregion
+
+
     }
 }
