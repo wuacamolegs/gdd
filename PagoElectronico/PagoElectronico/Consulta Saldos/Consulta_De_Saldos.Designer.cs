935f1fb3d4bb135976cfd2c2024f05e2e3b55385
flormaldonado <mariaflorenciamaldonado@gmail.com>
2015-07-02 16:54:49 -0300

    Consulta de saldos (incompleto)

--- PagoElectronico/PagoElectronico/Consulta Saldos/Consulta_De_Saldos.Designer.cs
+++ PagoElectronico/PagoElectronico/Consulta Saldos/Consulta_De_Saldos.Designer.cs
@@ -28,19 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.cmbCliente = new System.Windows.Forms.ComboBox();
+            this.cmbCuenta = new System.Windows.Forms.ComboBox();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.lblCuenta = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.btnConsultar.Location = new System.Drawing.Point(220, 166);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(167, 47);
+            this.btnConsultar.TabIndex = 0;
+            this.btnConsultar.Text = "CONSULTAR SALDO";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // cmbCliente
+            // 
+            this.cmbCliente.FormattingEnabled = true;
+            this.cmbCliente.Location = new System.Drawing.Point(170, 44);
+            this.cmbCliente.Name = "cmbCliente";
+            this.cmbCliente.Size = new System.Drawing.Size(217, 21);
+            this.cmbCliente.TabIndex = 1;
+            // 
+            // cmbCuenta
+            // 
+            this.cmbCuenta.FormattingEnabled = true;
+            this.cmbCuenta.Location = new System.Drawing.Point(170, 104);
+            this.cmbCuenta.Name = "cmbCuenta";
+            this.cmbCuenta.Size = new System.Drawing.Size(217, 21);
+            this.cmbCuenta.TabIndex = 2;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.lblCliente.Location = new System.Drawing.Point(42, 47);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(70, 18);
+            this.lblCliente.TabIndex = 3;
+            this.lblCliente.Text = "CLIENTE";
+            // 
+            // lblCuenta
+            // 
+            this.lblCuenta.AutoSize = true;
+            this.lblCuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.lblCuenta.Location = new System.Drawing.Point(43, 104);
+            this.lblCuenta.Name = "lblCuenta";
+            this.lblCuenta.Size = new System.Drawing.Size(69, 18);
+            this.lblCuenta.TabIndex = 4;
+            this.lblCuenta.Text = "CUENTA";
             // 
             // Consulta_De_Saldos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(695, 428);
+            this.ClientSize = new System.Drawing.Size(431, 243);
+            this.Controls.Add(this.lblCuenta);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.cmbCuenta);
+            this.Controls.Add(this.cmbCliente);
+            this.Controls.Add(this.btnConsultar);
             this.Name = "Consulta_De_Saldos";
             this.Text = "PAGO ELECTRONICO - CONSULTA DE SALDOS";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.ComboBox cmbCliente;
+        private System.Windows.Forms.ComboBox cmbCuenta;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label lblCuenta;
     }
 }
--- PagoElectronico/PagoElectronico/Consulta Saldos/Consulta_De_Saldos.cs
+++ PagoElectronico/PagoElectronico/Consulta Saldos/Consulta_De_Saldos.cs
@@ -6,14 +6,20 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Configuration;
 using Clases;
+using Utilities;
 
 namespace PagoElectronico.Consulta_Saldos
 {
     public partial class Consulta_De_Saldos : Form
     {
         #region variables
+
         public Usuario unUsuario = new Usuario();
+        public Cliente unCliente;
+        public Cuenta unaCuenta = new Cuenta();
+
         #endregion
 
         #region initialize
@@ -27,15 +33,66 @@ namespace PagoElectronico.Consulta_Saldos
             unUsuario = user;
             this.Show();
         }
+
+        private void Consulta_De_Saldos_Load(object sender, EventArgs e)
+        {
+            DataSet dsClientes = ObtenerClientes();
+            DropDownListManager.CargarCombo(cmbCliente, dsClientes.Tables[0], "cliente_id", "cliente_nombre", false, "");
+           
+        }
+
+        
+        
+        
         #endregion
 
         #region botones y vistas
+
+        
+        private void ComboCliente_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //cargar CMB cuenta
+
+            unaCuenta.cliente.cliente_id = Convert.ToInt32(cmbCliente.SelectedValue);
+            DataSet dsCuenta = unaCuenta.TraerCuentasActivasPorClienteID();
+            DropDownListManager.CargarCombo(cmbCuenta, dsCuenta.Tables[0], "cuenta_id", "cuenta_id", false, "");
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e) // TODO HACER
+        {
+
+        }
+
         #endregion
+
+        
 
         #region llamados a la base
         #endregion
 
         #region metodos privados
+        private DataSet ObtenerClientes()
+        {
+
+            DataSet ds = new DataSet();
+            if (unUsuario.Rol.rol_id == 1)
+            {
+                DataSet dsClientes = unCliente.ObtenerTodosLosClientes(unUsuario.usuario_id);
+                ds = dsClientes;
+            }
+            else
+            {
+                DataSet dsClienteUsuario = unCliente.ObtenerClientesPorUsuarioID(unUsuario.usuario_id);
+                ds = dsClienteUsuario;
+            }
+
+            return ds;
+
+        }
+
+
+
         #endregion
     }
 }
