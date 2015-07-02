7459a6bc0b76af6574de2cc19f9cbf6447a08f3b
wuacamolegs <wuacamolegs@gmail.com>
2015-07-01 21:44:20 -0300

    Procedures, agrego begin y end

--- 5-CrearStoredProceduresCAMI.sql
+++ 5-CrearStoredProceduresCAMI.sql
@@ -54,31 +54,39 @@ GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].traerUsuarioActivoPorUsername
     @Username nvarchar(255)
-AS 
+AS
+BEGIN 
     SELECT *
     FROM OOZMA_KAPPA.Usuario
     WHERE usuario_username = @Username AND usuario_estado = 0;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].traerListadoRolesPorId_Usuario
 	@usuario_id nvarchar(255)
 AS
+BEGIN
 	SELECT r.rol_id as rol_id, r.rol_nombre as rol_nombre , r.rol_estado as rol_estado FROM OOZMA_KAPPA.Usuario_rol ur, OOZMA_KAPPA.Rol r WHERE usuario_id = @usuario_id  AND ur.rol_id = r.rol_id;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].traerListadoFuncionalidadesPorId_Rol
     @id_Rol int
-AS 
+AS
+BEGIN 
     SELECT funcionalidades_id, funcionalidades_nombre 
     FROM OOZMA_KAPPA.Funcionalidades f , OOZMA_KAPPA.Funcionalidades_rol fr
     WHERE f.funcionalidades_id = fr.funcionalidad_id AND fr.rol_id = @id_Rol;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[deshabilitarUsuario]
 	@usuario_id int
 AS
+BEGIN
 	UPDATE [OOZMA_KAPPA].Usuario SET usuario_estado = 1 WHERE usuario_id = @usuario_id;
 	
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[updateUsuario]
@@ -87,30 +95,40 @@ CREATE PROCEDURE [OOZMA_KAPPA].[updateUsuario]
 	@Clave nvarchar(255),
 	@Estado bit
 AS
+BEGIN
 	UPDATE [OOZMA_KAPPA].Usuario SET usuario_username=@Username, usuario_password=@Clave, usuario_estado = @Estado WHERE usuario_id=@id_Usuario	
+END
 GO
 
 
 CREATE PROCEDURE [OOZMA_KAPPA].[traerListadoRoles]
 AS
+BEGIN
 	SELECT rol_id as id_Rol, rol_nombre as Nombre FROM OOZMA_KAPPA.Rol;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[traerListadoClienteCompleto]
 AS
+BEGIN
 	SELECT cliente_id as cliente_id,(cliente_apellido +' '+ cliente_nombre) as cliente_nombre, cliente_numero_documento as cliente_documento FROM OOZMA_KAPPA.Cliente;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[traerListadoClientePorUsuarioID]
 	@usuario_id numeric(18,0)
 AS
+BEGIN
 	SELECT cliente_id as cliente_id,(cliente_apellido +' '+ cliente_nombre) as cliente_nombre, cliente_numero_documento as cliente_documento FROM OOZMA_KAPPA.Cliente WHERE cliente_usuario_id = @usuario_id;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[traerListadoClientePorClienteID]
 	@cliente_id numeric(18,0)
 AS
+BEGIN
 	SELECT cliente_id, cliente_nombre, cliente_apellido, cliente_numero_documento, cliente_fecha_nacimiento FROM OOZMA_KAPPA.Cliente WHERE cliente_id = @cliente_id;
+END
 GO
 
 
@@ -118,25 +136,33 @@ CREATE PROCEDURE [OOZMA_KAPPA].[traerListadoCuentaActivasPorClienteID]
 	@cliente_id numeric(18,0),
 	@Fecha datetime
 AS
+BEGIN
 	SELECT cuenta_id as cuenta_numero, cuenta_estado, cuenta_saldo, cuenta_fecha_apertura as fecha_apertura, cuenta_fecha_cierre as fecha_cierre FROM OOZMA_KAPPA.Cuenta WHERE cuenta_cliente_id = @cliente_id AND cuenta_estado = 1 AND DATEDIFF(YEAR, cuenta_fecha_cierre, @Fecha) >= 0 AND DATEDIFF(MONTH, cuenta_fecha_cierre, @Fecha) >= 0;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[traerListadoCuentaPorClienteID]
 	@cliente_id numeric(18,0)
 AS
+BEGIN
 	SELECT cuenta_id as cuenta_numero, cuenta_estado, cuenta_saldo, cuenta_fecha_apertura as fecha_apertura, cuenta_fecha_cierre as fecha_cierre FROM OOZMA_KAPPA.Cuenta WHERE cuenta_cliente_id = @cliente_id;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[traerListadoCuentaporCuentaID]
 	@cuenta_id numeric(18,0)
 AS
+BEGIN
 	SELECT * FROM [OOZMA_KAPPA].Cuenta ;
+END
 GO
 
 
 CREATE PROCEDURE [OOZMA_KAPPA].[traerListadoBancoCompleto]
 AS
+BEGIN
 	SELECT banco_id, (banco_nombre + ' Sucursal : ' + CAST(banco_id as varchar)) as banco_nombre FROM OOZMA_KAPPA.Banco;
+END
 GO
 
 
@@ -147,11 +173,13 @@ CREATE PROCEDURE [OOZMA_KAPPA].[insertCheque_RetornarID]
 	@cheque_fecha dateTime,
 	@cheque_importe numeric(18,0)
 AS
+BEGIN
 	INSERT INTO OOZMA_KAPPA.Cheque(cheque_banco_id,cheque_cuenta_id,cheque_destino_cliente_id,cheque_fecha,cheque_importe)
 	VALUES (@cheque_banco_id,@cheque_cuenta_id,@cheque_cliente_id,@cheque_fecha,@cheque_importe );
 	
-	SELECT @@IDENTITY AS cheque_id;
+	SELECT @@IDENTITY as cheque_id;
 
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[insertRetiro_RetornarID]
@@ -160,49 +188,62 @@ CREATE PROCEDURE [OOZMA_KAPPA].[insertRetiro_RetornarID]
 	@retiro_fecha dateTime,
 	@retiro_importe numeric(18,0)
 AS
+BEGIN
 	INSERT INTO OOZMA_KAPPA.Retiro(retiro_cheque_id, retiro_cuenta_id, retiro_fecha, retiro_importe) 
 	VALUES (@retiro_cheque_id, @retiro_cuenta_id, @retiro_fecha, @retiro_importe) ;
 	
-	SELECT @@IDENTITY AS retiro_id;
+	SELECT @@IDENTITY as retiro_id;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[TraerListadoClienteTransferenciasAFacturar]
 	@cliente_id numeric(18,0)
-AS	
+AS
+BEGIN	
 	SELECT transaccion_pendiente_id , transaccion_pendiente_cliente_id, transaccion_pendiente_fecha, transaccion_pendiente_importe FROM OOZMA_KAPPA.Transacciones_Pendientes WHERE transaccion_pendiente_cliente_id = @cliente_id AND (transaccion_pendiente_descr = 'Comision por transferencia');
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[TraerListadoClienteSuscripcionesPendientesAFacturarPorClienteIDYCuentaID]
 	@cliente_id numeric(18,0),
 	@cuenta_id numeric(18,0)
 AS
+BEGIN
 	SELECT transaccion_pendiente_cliente_id,transaccion_pendiente_cuenta_id, transaccion_pendiente_fecha, transaccion_pendiente_importe FROM OOZMA_KAPPA.Transacciones_Pendientes WHERE transaccion_pendiente_cliente_id = @cliente_id AND transaccion_pendiente_cuenta_id = @cuenta_id AND (transaccion_pendiente_descr = 'Suscripciones por Apertura Cuenta');
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[TraerListadoClienteModificacionesTCAFacturar]
 	@cliente_id numeric(18,0)
 AS
+BEGIN
 	SELECT transaccion_pendiente_id ,transaccion_pendiente_cliente_id,transaccion_pendiente_fecha, transaccion_pendiente_importe FROM OOZMA_KAPPA.Transacciones_Pendientes WHERE transaccion_pendiente_cliente_id = @cliente_id AND (transaccion_pendiente_descr = 'Modificaciones Tipo Cuenta');
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[TraerListadoClienteCantidadSuscripcionesPendientesAFacturarPorClienteIDYCuentaID]
 	@cliente_id numeric(18,0),
 	@cuenta_id numeric(18,0)
-AS 
+AS
+BEGIN 
 	SELECT COUNT(transaccion_pendiente_cliente_id) as cantidadSuscripciones FROM OOZMA_KAPPA.Transacciones_Pendientes WHERE transaccion_pendiente_cliente_id = @cliente_id AND transaccion_pendiente_descr = 'Suscripciones por Apertura Cuenta' AND transaccion_pendiente_cuenta_id = @cuenta_id GROUP BY transaccion_pendiente_cliente_id
 
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[TraerListadoCuentaAPagarPorClienteID]
 	@cliente_id numeric(18,0)
-AS 
+AS
+BEGIN 
 	SELECT DISTINCT transaccion_pendiente_cuenta_id as cuenta_id FROM OOZMA_KAPPA.Transacciones_Pendientes WHERE transaccion_pendiente_cliente_id = @cliente_id;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[InsertItem_Factura]
 	@item_factura_numero numeric(18,0),
 	@item_factura_tabla_items TVP_Items READONLY
 	AS
+BEGIN
 	 DECLARE @item_descr varchar(255)
 	 DECLARE @item_cantidad numeric(18,0)
 	 DECLARE @item_costo numeric(18,2)
@@ -236,6 +277,7 @@ CREATE PROCEDURE [OOZMA_KAPPA].[InsertItem_Factura]
 	 
 	 CLOSE itemsCursor;
 	 DEALLOCATE itemsCursor;
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[InsertFactura_RetornarID]
@@ -245,6 +287,7 @@ CREATE PROCEDURE [OOZMA_KAPPA].[InsertFactura_RetornarID]
 	@factura_id numeric(18,0) = NULL OUTPUT,
 	@tablaSuscripciones TVP_SuscripcionesABorrar READONLY	
 AS
+BEGIN
 	 DECLARE @Cliente numeric(18,0)
 	 DECLARE @Cuenta numeric(18,0)
 	 DECLARE @CantidadSuscripciones int
@@ -277,13 +320,16 @@ AS
 	
 	SET @factura_id = @@IDENTITY;
 
+END
 GO
 
 CREATE PROCEDURE [OOZMA_KAPPA].[DeleteSuscripcionesAfterFacturacion]
 	@cuenta_id numeric(18,0),
 	@cantidadSuscripciones numeric(18,0)
 AS
+BEGIN
 	DELETE FROM OOZMA_KAPPA.Transacciones_Pendientes WHERE transaccion_pendiente_cuenta_id = @cuenta_id;
 
+END
 GO
 
