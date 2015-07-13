a1121ecbb7d79f8179fff63165d404099327a1ca
Guillermo Santillan <guillermosantillan1437847@yahoo.com>
2015-07-12 23:48:22 -0300

    Listado estadistico Designer

--- TP1C2015 K3013 OOZMA_KAPPA 33/src/PagoElectronico/Listados/Listado_Estadistico.Designer.cs
+++ TP1C2015 K3013 OOZMA_KAPPA 33/src/PagoElectronico/Listados/Listado_Estadistico.Designer.cs
@@ -29,11 +29,6 @@
         private void InitializeComponent()
         {
             this.dtgClientes = new System.Windows.Forms.DataGridView();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
             this.btnFiltrado1 = new System.Windows.Forms.Button();
             this.btnFiltrado5 = new System.Windows.Forms.Button();
             this.btnFiltrado4 = new System.Windows.Forms.Button();
@@ -44,7 +39,17 @@
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtgClientes)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
@@ -54,76 +59,21 @@
             // dtgClientes
             // 
             this.dtgClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgClientes.Location = new System.Drawing.Point(12, 231);
+            this.dtgClientes.Location = new System.Drawing.Point(12, 268);
             this.dtgClientes.Name = "dtgClientes";
             this.dtgClientes.Size = new System.Drawing.Size(658, 159);
             this.dtgClientes.TabIndex = 11;
             // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(31, 12);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(481, 17);
-            this.radioButton1.TabIndex = 19;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Clientes que algunas de sus cuentas fueron inhabilitadas por no pagar los costos " +
-                "de transaccion.";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(19, 17);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(383, 17);
-            this.radioButton2.TabIndex = 20;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Cliente con mayor cantidad de comisiones facturadas en todas sus cuentas.";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(19, 17);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(400, 17);
-            this.radioButton3.TabIndex = 21;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Clientes con mayor cantidad de transacciones realizadas entre cuentas propias.";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(19, 17);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(369, 17);
-            this.radioButton4.TabIndex = 22;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Paises con mayor cantidad de movimientos tanto ingresos como egresos.";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // radioButton5
-            // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(19, 17);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(262, 17);
-            this.radioButton5.TabIndex = 23;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Total facturado para los distInt64os tipos de cuentas.";
-            this.radioButton5.UseVisualStyleBackColor = true;
-            // 
             // btnFiltrado1
             // 
             this.btnFiltrado1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFiltrado1.Location = new System.Drawing.Point(538, 12);
+            this.btnFiltrado1.Location = new System.Drawing.Point(526, 11);
             this.btnFiltrado1.Name = "btnFiltrado1";
             this.btnFiltrado1.Size = new System.Drawing.Size(72, 23);
             this.btnFiltrado1.TabIndex = 24;
             this.btnFiltrado1.Text = "Filtrado 1";
             this.btnFiltrado1.UseVisualStyleBackColor = true;
+            this.btnFiltrado1.Click += new System.EventHandler(this.btnFiltrado1_Click);
             // 
             // btnFiltrado5
             // 
@@ -167,7 +117,9 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnFiltrado1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(632, 40);
             this.groupBox1.TabIndex = 30;
@@ -175,9 +127,9 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.btnFiltrado2);
-            this.groupBox2.Controls.Add(this.radioButton2);
-            this.groupBox2.Location = new System.Drawing.Point(12, 47);
+            this.groupBox2.Location = new System.Drawing.Point(12, 87);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(632, 40);
             this.groupBox2.TabIndex = 31;
@@ -185,9 +137,9 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Controls.Add(this.label5);
             this.groupBox3.Controls.Add(this.btnFiltrado3);
-            this.groupBox3.Location = new System.Drawing.Point(12, 93);
+            this.groupBox3.Location = new System.Drawing.Point(12, 133);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(632, 40);
             this.groupBox3.TabIndex = 31;
@@ -195,9 +147,9 @@
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.radioButton4);
+            this.groupBox4.Controls.Add(this.label6);
             this.groupBox4.Controls.Add(this.btnFiltrado4);
-            this.groupBox4.Location = new System.Drawing.Point(12, 139);
+            this.groupBox4.Location = new System.Drawing.Point(12, 179);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(632, 40);
             this.groupBox4.TabIndex = 31;
@@ -205,21 +157,102 @@
             // 
             // groupBox5
             // 
-            this.groupBox5.Controls.Add(this.radioButton5);
+            this.groupBox5.Controls.Add(this.label7);
             this.groupBox5.Controls.Add(this.btnFiltrado5);
-            this.groupBox5.Location = new System.Drawing.Point(12, 185);
+            this.groupBox5.Location = new System.Drawing.Point(12, 225);
             this.groupBox5.Name = "groupBox5";
             this.groupBox5.Size = new System.Drawing.Size(632, 40);
             this.groupBox5.TabIndex = 31;
             this.groupBox5.TabStop = false;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(292, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 32;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(69, 10);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 33;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Año";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Trimestre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(463, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Clientes que algunas de sus cuentas fueron inhabilitadas por no pagar los costos " +
+                "de transaccion.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(365, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Cliente con mayor cantidad de comisiones facturadas en todas sus cuentas.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(382, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Clientes con mayor cantidad de transacciones realizadas entre cuentas propias.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(351, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Paises con mayor cantidad de movimientos tanto ingresos como egresos.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(244, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Total facturado para los distintos tipos de cuentas.";
+            // 
             // Listado_Estadistico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 393);
-            this.Controls.Add(this.btnFiltrado1);
-            this.Controls.Add(this.radioButton1);
+            this.ClientSize = new System.Drawing.Size(682, 439);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dtgClientes);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
@@ -229,6 +262,8 @@
             this.Name = "Listado_Estadistico";
             this.Text = "PAGO ELECTRONICO - LISTADO ESTADISTICO";
             ((System.ComponentModel.ISupportInitialize)(this.dtgClientes)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
@@ -245,11 +280,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dtgClientes;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton5;
         private System.Windows.Forms.Button btnFiltrado1;
         private System.Windows.Forms.Button btnFiltrado5;
         private System.Windows.Forms.Button btnFiltrado4;
@@ -260,5 +290,14 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
